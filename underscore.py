1188e90d590e11045bb337f1737f2d96e7c7d5f4
dakr <dappili.anilreddy@gmail.com>
2017-01-05 21:34:06 +0530

    under score examples

---
+++ underscore.py
@@ -0,0 +1,28 @@
+class A:
+	def __init__(self):
+		print ('from anill')
+	# _anil = 10
+	# print (_anil)
+	# def __method1(self):
+	# 	print('hello from method1')
+
+
+	# def method2(self):
+	# 	return self.__method1()
+
+# class B(A):
+# 	def method3(self):
+# 		print('from method3')
+
+# 	def __method1(self):
+# 		print('hello from method1 from B calss')
+
+
+a = A()
+# pr
+int(a)
+# b = B()
+# # print(dir(A()))
+# print(dir(B()))
+
+# b = B()
